1a155b24489ff7f4047cba3fd6f0d5ec16507ebe
HienTruong <hien0934752432@gmail.com>
2025-05-30 20:24:35 +0700

    adding chappter / - 17

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,23 +3,23 @@ import Link from "next/link";
 import Layout from "../../components/layout";
 import Script from "next/script";
 
-export default function FirstPost(){
-    return (
-        <Layout>
-            <Head>
-                <title>First Post title</title>
-                <Script
-                    src="https://connect.facebook.net/en_US/sdk.js"
-                    strategy="lazyOnload"
-                    onLoad={ () => 
-                        console.log(`script loaded correctly, window.FB has been populated`)
-                    }
-                ></Script>
-                
-            </Head>
-            <h1>First Post</h1>
-            <h2>Back To <Link href="/">Homepage</Link></h2>
-        </Layout>
-
-    );
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 }
