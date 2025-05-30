1cae5b0734f8814a222d314e10e43f81adce0f13
HienTruong <hien0934752432@gmail.com>
2025-05-30 13:56:52 +0700

    Adding chappter 14 and resolve node: install and use node v20

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,25 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Script from "next/script";
 
 export default function FirstPost(){
     return (
         <Layout>
             <Head>
                 <title>First Post title</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={ () => 
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                ></Script>
+                
             </Head>
             <h1>First Post</h1>
             <h2>Back To <Link href="/">Homepage</Link></h2>
         </Layout>
+
     );
 }
