8269889ee75762c30ebc02e95f6d26cb0718848e
HienTruong <hien0934752432@gmail.com>
2025-05-31 23:39:35 +0700

    done chapter 29

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
-import utilStyle from '../styles/utils.module.css';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/post';
 
-export default function Home(){
+// getSortedPostsData giống như hàm để đọc tất cả các file cua next-blog - lấy ra các giá trị và trả về { }.
+// Bây giờ tạo 1 Static - Generation: 
+//  Giành cho các web tĩnh, giống như việc bạn có sẵn một page đang chờ sẵn, và chỉ chờ bạn click để hiện ra.
+// Không bao giờ có ở client. 
+// Đây là hàm để lấy các
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return { 
+    props: { 
+      allPostsData, },
+    };
+}
+
+
+export default function Home({allPostsData}){
   return (
     // Layout home tức là đang cho giá trị true - đối chiếu với bên layout
     <Layout home> 
     <Head>
       <title>{siteTitle}</title>
     </Head>
-    <section className={utilStyle.headingMd}>
+    <section className={utilStyles.headingMd}>
       <p>
         Hello, My name is Trương Hoàng Minh Hiển. 
         I want to become A fullstack Dev
@@ -19,6 +34,21 @@ export default function Home(){
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
       </p>
     </section>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
   </Layout>
+  
   );
 }
